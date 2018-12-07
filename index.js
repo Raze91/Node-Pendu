fb96fa3c26bb6450da3d767faae70323633e42ef
Sami Hattab <hattab_sami@yahoo.fr>
2018-12-07 10:07:05 +0100

    first origin commit

---
+++ index.js
@@ -0,0 +1,8 @@
+let  MotATrouver= [];
+
+let  mots = ['STYLO','SOLEIL','ZOMBIE','TANK','MANUFACTURE','POMPIER'];
+
+for(let i = 0; i < mots.length; i++){
+    MotATrouver = mots[i];
+    console.log(MotATrouver);    
+}
