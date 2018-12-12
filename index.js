8bdd19b81db2bb64d9f2d1ff8f97e5c47475aec4
Sami Hattab <hattab_sami@yahoo.fr>
2018-12-12 16:22:59 +0100

    Première Version Jouable

--- index.js
+++ index.js
@@ -6,14 +6,46 @@ const inquirer = require('inquirer');
 
 const questions = require('./questions');
 
-let MotATrouver= [];
-let MotMystere = [];
-let nombre = 3;
+const mots = ['TANK','ABUS','STYLO','AVRIL','CHIENS','SOLEIL','ZOMBIE','POMPIER','VOYANCE','BASEBALL','CAVALIER','TURQUOISE','TROMPETTE',
+'ABSOLUTION','TRANSFERTS','BIJOUTIERE','MANUFACTURE','UNILATERAUX'];
 
-let  mots = ['TANK','ABUS','STYLO','AVRIL','CHIENS','SOLEIL','ZOMBIE','POMPIER','VOYANCE','BASEBALL','CAVALIER'
-,'TURQUOISE','TROMPETTE','ABSOLUTION','TRANSFERTS','BIJOUTIERE','MANUFACTURE','UNILATERAUX'];
+const mots4 = ['TANK','ABUS'];
 
-let Mot = shuffle(mots);
+const mots5 = ['STYLO','AVRIL'];
+
+const mots6 = ['CHIENS','SOLEIL','ZOMBIE'];
+
+const mots7 = ['POMPIER','VOYANCE'];
+
+const mots8 = ['BASEBALL','CAVALIER'];
+
+const mots9 = ['TURQUOISE','TROMPETTE'];
+
+const mots10 = ['ABSOLUTION','TRANSFERTS','BIJOUTIERE'];
+
+const mots11 = ['MANUFACTURE','UNILATERAUX'];
+
+const Mots = shuffle(mots);
+
+const Mots4 = shuffle(mots4);
+
+const Mots5 = shuffle(mots5);
+
+const Mots6 = shuffle(mots6);
+
+const Mots7 = shuffle(mots7);
+
+const Mots8 = shuffle(mots8);
+
+const Mots9 = shuffle(mots9);
+
+const Mots10 = shuffle(mots10);
+
+const Mots11 = shuffle(mots11);
+let MotATrouver= Mots[3];
+const data = MotATrouver.split('');
+let MotMystere = '';
+const letters = [];
 
 const App = {
     title : figlet.textSync('Node-Pendu','Ghost'),
@@ -26,19 +58,104 @@ const App = {
 
 App.logTitle()
 
+function AskQuestion(){
+    inquirer.prompt(questions).then(reponsesUtilisateur => {
+        MotMystere = '';
+        for(let key in reponsesUtilisateur){
+            if(reponsesUtilisateur[key] === 'A'){
+                letters.push('A');
+            };
+            if(reponsesUtilisateur[key] === 'B'){
+                letters.push('B');
+            };
+            if(reponsesUtilisateur[key] === 'C'){
+                letters.push('C');
+            };
+            if(reponsesUtilisateur[key] === 'D'){
+                letters.push('D');
+            };
+            if(reponsesUtilisateur[key] === 'E'){
+                letters.push('E');
+            };
+            if(reponsesUtilisateur[key] === 'F'){
+                letters.push('F');
+            };
+            if(reponsesUtilisateur[key] === 'G'){
+                letters.push('G');
+            };
+            if(reponsesUtilisateur[key] === 'H'){
+                letters.push('H');
+            };
+            if(reponsesUtilisateur[key] === 'I'){
+                letters.push('I');
+            };
+            if(reponsesUtilisateur[key] === 'J'){
+                letters.push('J');
+            };
+            if(reponsesUtilisateur[key] === 'K'){
+                letters.push('K');
+            };
+            if(reponsesUtilisateur[key] === 'L'){
+                letters.push('L');
+            };
+            if(reponsesUtilisateur[key] === 'M'){
+                letters.push('M');
+            };
+            if(reponsesUtilisateur[key] === 'N'){
+                letters.push('N');
+            };
+            if(reponsesUtilisateur[key] === 'O'){
+                letters.push('O');
+            };
+            if(reponsesUtilisateur[key] === 'P'){
+                letters.push('P');
+            };
+            if(reponsesUtilisateur[key] === 'Q'){
+                letters.push('Q');
+            };
+            if(reponsesUtilisateur[key] === 'R'){
+                letters.push('R');
+            };
+            if(reponsesUtilisateur[key] === 'S'){
+                letters.push('S');
+            };
+            if(reponsesUtilisateur[key] === 'T'){
+                letters.push('T');
+            };
+            if(reponsesUtilisateur[key] === 'U'){
+                letters.push('U');
+            };
+            if(reponsesUtilisateur[key] === 'V'){
+                letters.push('V');
+            };
+            if(reponsesUtilisateur[key] === 'W'){
+                letters.push('W');
+            };
+            if(reponsesUtilisateur[key] === 'X'){
+                letters.push('X');
+            };
+            if(reponsesUtilisateur[key] === 'Y'){
+                letters.push('Y');
+            };
+            if(reponsesUtilisateur[key] === 'Z'){
+                letters.push('Z');
+            };
+            for (let j = 0; j < data.length; j++) {
+                if (letters.includes(data[j]) == true) {
+                MotMystere += data[j];
+                } else {
+                MotMystere += '-';
+                }
+            }
+        }
+        console.log('\n'+MotMystere);
+        if(MotMystere === MotATrouver){
+            console.log(figlet.textSync('Bravo !','Ogre'));
+        } else {
+            AskQuestion();
+        }
+    });
+}
 
-MotATrouver = Mot[nombre];
+AskQuestion();
 
-console.log(MotATrouver);
-
-let regex = MotATrouver
-let tiret = '-';
-
-MotMystere = MotATrouver.replace(regex, tiret.repeat(MotATrouver.length));
-
-console.log(MotMystere);
-
-
-inquirer.prompt(questions).then(reponsesUtilisateur => {
-    
-});
--- questions.js
+++ questions.js
@@ -1,19 +1,19 @@
 module.exports = [
-    {
-    type: 'list',
-        name: 'question_1',
-        message: 'Choisissez la longueur du mot.',
-        choices: [
-            '4',
-            '5',
-            '6',
-            '7',
-            '8',
-            '9',
-            '10',
-            '11',
-        ]
-    },
+    // {
+    // type: 'list',
+    //     name: 'question_1',
+    //     message: 'Choisissez la longueur du mot.',
+    //     choices: [
+    //         '4',
+    //         '5',
+    //         '6',
+    //         '7',
+    //         '8',
+    //         '9',
+    //         '10',
+    //         '11',
+    //     ]
+    // },
     {
         type: 'input',
         name: 'question_2',
